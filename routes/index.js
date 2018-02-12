55bee6f6c4f043cc07426347cd13c0ac157063d0
Cadick <s_cadick2@tss-hr420-dm07.fc.ca>
2018-02-12 13:41:12 -0500

    database

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mysql = require('mysql');
+var connect = require('../utils/sqlConnect')
 var router = express.Router();
 
 const templateTypes = [
@@ -9,10 +11,19 @@ const templateTypes = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
+
+  connect.query('SELECT * FROM mainmodel', (err, result, fields) => {
+    if (err) {
+      throw err; console.log(err);
+    } else {
+      console.log(result);
+    }
+  });
+  res.render('home', {
     title: 'Express',
-    message : "I really like EJS",
-    templates : templateTypes
+    message : "I like Handlebars",
+    templates : templateTypes,
+    anothermessage : 'handlebars is awesome'
  });
 });
 
